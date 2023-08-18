ded26e29dd317446552e321b831051c75b149985
hotvideo <63905283+hotvideo@users.noreply.github.com>
2023-08-18 21:08:47 +0700

    Delete 1981814.js

--- 1981814.js
+++
@@ -1,8 +0,0 @@
-self.options = {
-    "domain": "forlumineoner.com",
-    "resubscribeOnInstall": true,
-    "zoneId": 1981815,
-    "pub": 1
-}
-self.lary = "";
-importScripts('https://forlumineoner.com/pfe/current/service-worker.min.js?r=sw')
